91d3ff37104a8500e98cda1c95959db6596b05df
Derine Banis <d.banis.13@gmail.com>
2022-05-08 21:10:57 -0700

    halfway through my conditionals.js file on the prep module

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+const raining = false;
+const cold = false;
+
+if(raining){
+  console.log(`Don't forget your umbrella`);
+}
+
+if(cold){
+  console.log(`Make sure your pick out a scarf!`);
+}
+
+console.log(`Now you're ready to go outside!`);
