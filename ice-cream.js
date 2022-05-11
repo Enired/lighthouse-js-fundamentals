9f817967296197b08118acb25a3f90b11f8506ca
Derine Banis <d.banis.13@gmail.com>
2022-05-11 11:43:41 -0700

    prep module ice cream assignment

---
+++ ice-cream.js
@@ -0,0 +1,19 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+//Adding root beer flavor
+iceCreamFlavours.push(`root beer`);
+console.log(iceCreamFlavours);
+
+//Getting the first flavor
+console.log(iceCreamFlavours[0]);
+
+//Getting the last flavor
+console.log(iceCreamFlavours[iceCreamFlavours.length-1]);
+
+//Getting the total number of flavors
+console.log(iceCreamFlavours.length);
+
+
